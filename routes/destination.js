50cbd780cc2cdaa8e9befc188315b1996d7bd0f2
mlewis2257 <mlang.lewis@gmail.com>
2023-08-23 15:23:24 -0700

    show.ejs and destination

--- routes/destination.js
+++ routes/destination.js
@@ -2,6 +2,6 @@ var express = require("express");
 var router = express.Router();
 const destCtrl = require("../controllers/destinations");
 
-router.get("flights/:id/destination", destCtrl.create);
+router.post("/flights/:id/destinations", destCtrl.create);
 
 module.exports = router;
